58fc07f0450b7950a0fc15c89b315b049a3ccf99
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-01-12 20:55:58 +0000

    Update sql_merge.py

--- pages/sql_merge.py
+++ pages/sql_merge.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.write('### Update values, add new ones and delete')
-st.write('In 2016 SQL introduced the MERGE function allowing you to update values, add new ones and delete. This is a powerful all in one function which minimises and simplifies your code.
-I frequently use this for performing mapping exercises between two systems and keeping them in sync with each other.')
+st.write('In 2016 SQL introduced the MERGE function allowing you to update values, add new ones and delete. This is a powerful all in one function which minimises and simplifies your code.')
 st.write('### Set up code:')
 
