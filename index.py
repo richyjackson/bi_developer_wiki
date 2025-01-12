f98c1193e9116de6a37a75cce5b2f67060b56fd7
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-01-12 10:54:58 +0000

    Grammar change

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.write('## BI Developer WIKI')
-st.write('Find solutions to common problems a BI developers face')
+st.write('Find solutions to common problems BI developers face')
 
 
