e7f033d907ed63a221fe6e89fc497f16f04cbcea
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-01-12 21:39:27 +0000

    Update sql_merge.py

--- pages/sql_merge.py
+++ pages/sql_merge.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.write('### Update values, add new ones and delete')
 st.write('In 2016 SQL introduced the MERGE function allowing you to update values, add new ones and delete. This is a powerful all in one function which minimises and simplifies your code.')
 st.write('### Set up code:')
-
+st.write('It doesnt seem to be updating')
 code_block_1 = (
 "DECLARE @source AS TABLE (country VARCHAR(50), region_code VARCHAR(2))\n"\n
 "INSERT INTO @source VALUES (\'United Kingdom\', \'GB\')\n"
