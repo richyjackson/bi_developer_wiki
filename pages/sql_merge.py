9e3b0d03e1943448dba143c1b5a9ffe54d686d11
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-01-12 21:04:23 +0000

    Update sql_merge.py

--- pages/sql_merge.py
+++ pages/sql_merge.py
@@ -4,3 +4,6 @@ st.write('### Update values, add new ones and delete')
 st.write('In 2016 SQL introduced the MERGE function allowing you to update values, add new ones and delete. This is a powerful all in one function which minimises and simplifies your code.')
 st.write('### Set up code:')
 
+st.code("DECLARE @source AS TABLE (country VARCHAR(50), region_code VARCHAR(2))
+INSERT INTO @source VALUES ('United Kingdom', 'GB')
+INSERT INTO @source VALUES ('France', 'FR')", language = "SQL')
