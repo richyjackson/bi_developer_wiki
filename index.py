5993441a5e943f37622e8b8705210e50498edb38
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-03-17 22:16:35 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
+create_page = st.Page("app_pages/sql/sql_merge.py", title="Create entry", icon=":material/add_circle:")
+delete_page = st.Page("app_pages/sql/sql_merge.py", title="Delete entry", icon=":material/delete:")
+
+pg = st.navigation([create_page, delete_page])
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+pg.run()
+
 st.write('## BI Developer WIKI')
 st.write('Find solutions to common problems BI developers face')
 st.divider()
 st.write('#### SQL')
-
-st.sidebar.page_link("app_pages/sql/sql_merge.py", label="Merge")
