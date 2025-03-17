bc3c92c629838cd773ccfb3761f3377c5853903b
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-03-17 19:44:53 +0000

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ st.write('## BI Developer WIKI')
 st.write('Find solutions to common problems BI developers face')
 st.divider()
 st.write('#### SQL')
+st.sidebar(expanded=True)
 pages = [
   st.Page(page:"app_pages/sql/sql_merge.py", title="MERGE")
 ]
