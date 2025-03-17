072e95751764096353993f736f5bd5a49b27c731
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-03-17 22:22:01 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-create_page = st.Page("app_pages/sql/sql_merge.py", title="Create entry", icon=":material/add_circle:")
-delete_page = st.Page("app_pages/sql/sql_merge.py", title="Delete entry", icon=":material/delete:")
+create_page = st.Page("app_pages/sql/sql_merge_2.py", title="Create entry", icon=":material/add_circle:")
+delete_page = st.Page("app_pages/sql/sql_merge_2.py", title="Delete entry", icon=":material/delete:")
 
 pg = st.navigation([create_page, delete_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
