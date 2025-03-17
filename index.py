9ebedc489ee3ee1c8cdd449b83279b33b918567b
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-03-17 19:39:51 +0000

    Update index.py

--- index.py
+++ index.py
@@ -8,6 +8,6 @@ pages = [
   st.Page(page:"app_pages/sql/sql_merge.py", title = "MERGE")
 ]
 
-pg = st.navigation(pages, position = "sidebar", expanded = True)
+pg = st.navigation(pages, position="sidebar", expanded=True)
 
 pg.run()
