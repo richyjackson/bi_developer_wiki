dc79decd715e99d9ec233c29ff65966482ff2bc1
richyjackson <62892481+richyjackson@users.noreply.github.com>
2025-03-17 19:32:57 +0000

    Update index.py

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ st.write('## BI Developer WIKI')
 st.write('Find solutions to common problems BI developers face')
 st.divider()
 st.write('#### SQL')
-pages = [
-  st.page(page:"app_pages/sql/sql_merge.py", title = "MERGE")
-]
+#pages = [
+#  st.page(page:"app_pages/sql/sql_merge.py", title = "MERGE")
+#]
 
-pg = st.navigation(pages, position = "sidebar", expanded = True)
+#pg = st.navigation(pages, position = "sidebar", expanded = True)
 
-pg.run
+#pg.run
